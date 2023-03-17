d1b699a423c1a2e416dbe14fa18afd947bdf595c
Gabriel Kulp <kulpga@oregonstate.edu>
2023-03-17 12:22:59 -0700

    clipboard integration shellcheck

--- .config/distrobox/wl-clipboard-host.sh
+++ .config/distrobox/wl-clipboard-host.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-echo "success" > /success.txt
-#mkdir -p /usr/sbin
-#mkdir -p /usr/local/bin
-#ln -s /usr/sbin/distrobox-host-exec /usr/local/bin/wl-copy
-#ln -s /usr/sbin/distrobox-host-exec /usr/local/bin/wl-paste
-
-ln -s `type -P distrobox-host-exec` /usr/local/bin/wl-copy
-ln -s `type -P distrobox-host-exec` /usr/local/bin/wl-paste
+mkdir -p /usr/local/bin
+ln -s "$(type -P distrobox-host-exec)" /usr/local/bin/wl-copy
+ln -s "$(type -P distrobox-host-exec)" /usr/local/bin/wl-paste
